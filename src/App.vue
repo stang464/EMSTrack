c42565b259ae2ceee0095ee64cb9a1112fb23d2e
Phoatipong Musikong <phoatipong@gmail.com>
2020-09-24 23:24:37 +0700

    master

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,36 @@
 <template >
-  <div id="app" >
-  <!-- As a heading -->
-  <b-navbar variant="faded" type="light" class="bg-danger">
-    <b-navbar-brand tag="h1" class="mb-0 bg-danger"><H1 style="color:white">EMS TRACKING</H1></b-navbar-brand>
-  </b-navbar>
+  <div id="app">
+    <!-- As a heading -->
+    <b-navbar variant="faded" type="light" class="bg-danger">
+      <b-navbar-brand tag="h1"  class="mb-0 bg-danger"
+        ><H1 style="font-family: 'IBM Plex Mono', monospace;
+ color: white">EMS TRACKING</H1></b-navbar-brand
+      >
+    </b-navbar>
 
-  <div class="container">
-    <div class="row mt-2">
-     <div class="col">
-       <Thaipost/>
-     </div>
-        
-      
-      
-      
+    <div class="container">
+      <div class="row mt-2">
+        <div class="col">
+          <Thaipost />
+        </div>
+      </div>
     </div>
-  </div>
     
-</div>
-  
+  </div>
 </template>
 
 <script>
-
-import Thaipost from './components/Thaipost.vue'
+import Thaipost from "./components/Thaipost.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Thaipost
-  }
-}
+    Thaipost,
+  },
+};
 </script>
 
 <style>
-#app{
-  font-family: 'Prompt', sans-serif;
-
+#app {
+  font-family: "Prompt", sans-serif;
 }
-
-
-
 </style>
